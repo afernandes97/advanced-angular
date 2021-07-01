49a2f2f91a6e8d0ca83786181d7c225d6f6f235e
Fernandes <andre.a.fernandes@capgemini.com>
2021-07-01 15:53:27 -0300

    improving category list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { CategoryListComponent } from './pages/categories/category-list/category-list.component';
 import { CategoryFormComponent } from './pages/categories/category-form/category-form.component';
 import { NgModule } from '@angular/core';
@@ -6,7 +7,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { InMemoryDatabase } from "./in-memory-database";
 
@@ -20,6 +21,7 @@ import { InMemoryDatabase } from "./in-memory-database";
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,18 +1,39 @@
+import { CategoryService } from './../shared/category.service';
 import { Component, OnInit } from '@angular/core';
+
+import { Category } from "../shared/category.model";
+
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
+  providers: [CategoryService]
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(private categoService: CategoryService) { }
 
   ngOnInit(): void {
+    this.categoService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Erro ao carregar a lista')
+    )
   }
 
-  funcao(){
-    
+
+  deleteCategory(category: any){
+    const mustDelete = confirm("Deseja realmente excluir esse item?");
+
+    if(mustDelete){
+      this.categoService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert("Erro ao excluir")
+      )
+    }
+
   }
+
 }
